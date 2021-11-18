d9020167fb16c41dc8f281c75735149b226e1cbc
Minh Vuong <vuongxuongminh@gmail.com>
2021-11-18 11:47:59 +0700

    chore: remove copyright

--- src/Command/GetCountries.php
+++ src/Command/GetCountries.php
@@ -1,10 +1,4 @@
 <?php
-/*
- * (c) Minh Vuong <vuongxuongminh@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
 
 declare(strict_types=1);
 
